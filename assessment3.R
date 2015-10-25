edf2aff8feeb944c79eaf5eb2d9f9a3127e5ddf6
James Otto <jfotto2@gmail.com>
2015-10-25 16:42:03 -0400

    My solution to week 3 assignment

--- assessment3.R
+++ assessment3.R
@@ -15,55 +15,44 @@
 # function, but it seems to be creating a variable "x" that is not reachable 
 # from the global environment, but is available in the environment of the 
 # makeCacheMatrix function
-makeCacheMatrix <- function(x = numeric()) {
-        
-        # holds the cached value or NULL if nothing is cached
-        # initially nothing is cached so set it to NULL
-        cache <- NULL
-        
-        # store a matrix
-        setMatrix <- function(newValue) {
-                x <<- newValue
-                # since the matrix is assigned a new value, flush the cache
-                cache <<- NULL
-        }
+makeCacheMatrix <- function(x = numeric())
+{
+    cache_hold <- NULL
 
-        # returns the stored matrix
-        getMatrix <- function() {
-                x
-        }
+    setMatrix <- function(y)
+    {
+        x <<- y
+        cache_hold<<- NULL
+    }
 
-        # cache the given argument 
-        cacheInverse <- function(solve) {
-                cache <<- solve
-        }
+    getMatrix <- function()
+    {
+       x
+    }
 
-        # get the cached value
-        getInverse <- function() {
-                cache
-        }
-        
-        # return a list. Each named element of the list is a function
-        list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+    cacheInverse <- function(inverse)
+    {
+       cache_hold <<- inverse
+    }
+
+    getInverse <- function()
+    {
+         cache_hold
+    } 
+
+    list ( setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
-
-# The following function calculates the inverse of a "special" matrix created with 
-# makeCacheMatrix
-cacheSolve <- function(y, ...) {
-        # get the cached value
-        inverse <- y$getInverse()
-        # if a cached value exists return it
-        if(!is.null(inverse)) {
-                message("getting cached data")
-                return(inverse)
-        }
-        # otherwise get the matrix, caclulate the inverse and store it in
-        # the cache
-        data <- y$getMatrix()
-        inverse <- solve(data)
-        y$cacheInverse(inverse)
-        
-        # return the inverse
-        inverse
+cacheSolve <- function(x,...)
+{
+    inv <- x$getInverse()
+    if ( !is.null(inv))
+    {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$getMatrix()
+    inv <- solve(data)
+    x$cacheInverse(inv)
+    inv
 }
